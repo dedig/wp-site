8f6ccab2fb121cdf551457ba6a875def6db1ed21
Bruno Araujo <araujo.contato@gmail.com>
2015-11-03 16:16:03 -0200

    alterações do dev para master.

--- wp-content/themes/dedig/footer.php
+++ wp-content/themes/dedig/footer.php
@@ -35,7 +35,7 @@
 		</div>
 	</footer>
 	</div>
-	<script type="text/javascript" src="<?php bloginfo('template_directory'); ?>/js/functions.js"></script>
+	<script type="text/javascript" src="<?php bloginfo('template_directory'); ?>/js/functions.min.js"></script>
 	<?php wp_footer(); ?>
 </body>
 </html>
