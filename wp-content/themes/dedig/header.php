e1efb59b6db0f1ecc31ddc23f5c91117a2392d24
Bruno Araujo <araujo.contato@gmail.com>
2016-07-18 12:25:07 -0300

    Implementação completa do chat Livehelper

--- wp-content/themes/dedig/header.php
+++ wp-content/themes/dedig/header.php
@@ -38,12 +38,12 @@
 	<?php wp_head(); ?>
 	<script type="text/javascript">
 		var LHCChatOptions = {};
-		LHCChatOptions.opt = {widget_height:340,widget_width:300,popup_height:520,popup_width:500,domain:'52.67.97.178'};
+		LHCChatOptions.opt = {widget_height:340,widget_width:300,popup_height:520,popup_width:500,domain:'dedig.com.br'};
 		(function() {
 		var po = document.createElement('script'); po.type = 'text/javascript'; po.async = true;
 		var referrer = (document.referrer) ? encodeURIComponent(document.referrer.substr(document.referrer.indexOf('://')+1)) : '';
 		var location  = (document.location) ? encodeURIComponent(window.location.href.substring(window.location.protocol.length)) : '';
-		po.src = '//52.67.108.96/livehelper/lhc_web/index.php/por/chat/getstatus/(click)/internal/(position)/bottom_right/(ma)/br/(top)/350/(units)/pixels/(leaveamessage)/true/(department)/1/(disable_pro_active)/true?r='+referrer+'&l='+location;
+		po.src = '//otrs.dedig.com.br/dedig-chat/lhc_web/index.php/por/chat/getstatus/(click)/internal/(position)/bottom_right/(ma)/br/(top)/350/(units)/pixels/(leaveamessage)/true/(disable_pro_active)/true?r='+referrer+'&l='+location;
 		var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(po, s);
 		})();
 	</script>
